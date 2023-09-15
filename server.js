db8973e74d73e39c7da83b8de02dbebf3ff686fb
MuteeVincente <vinniemutee@gmail.com>
2023-09-15 15:20:53 +0300

    Final Commit

--- server.js
+++ server.js
@@ -23,4 +23,15 @@ app.post('/shortUrls', async (req, res) => {
     res.redirect('/');
 });
 
+app.get('/:shortUrl', async (req, res) => {
+    const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+    if (shortUrl == null) return res.sendStatus(404)
+  
+    shortUrl.clicks++
+    shortUrl.save()
+  
+    res.redirect(shortUrl.full)
+  })
+  
+
 app.listen(process.env.PORT || 5000);
