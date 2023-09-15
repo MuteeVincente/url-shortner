0f93d31a9550a01a258cc5022d81cd3d5f5bd10e
MuteeVincente <vinniemutee@gmail.com>
2023-09-15 15:01:35 +0300

    connecting to database

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
+require('dotenv').config(); // Load environment variables from .env file
+
 const express = require('express');
 const mongoose = require('mongoose');
+const ShortUrl = require('./models/shortUrl');
 const app = express();
 
-mongoose.connect('mongodb://localhost/urlShortener',{
-    useNewUrlParser: true, useUnifiedTopology:true
-})
+mongoose.connect(process.env.M_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req,res) =>{
-    res.render('index')
-})
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
+app.post('/shortUrls', async (req, res) => {
+    await ShortUrl.create({ full: req.body.fullUrl });
+    res.redirect('/');
+});
 
-app.post('/shortUrls', (req,res) =>{
-        
-})
-app.listen(process.env.PORT || 5000)
+app.listen(process.env.PORT || 5000);
